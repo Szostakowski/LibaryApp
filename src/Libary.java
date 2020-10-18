cdbd749fa28048d95d567222f02625e7b38b2a43
rafal <rafal.szostakowski@gmail.com>
2020-10-18 21:50:03 +0200

    Initial commit

---
+++ src/Libary.java
@@ -0,0 +1,18 @@
+public class Libary {
+    public static void main(String[] args) {
+        final String appName = "Biblioteka domowa v0.1";
+
+        String title = "Płuczki";
+        String author = "Paweł Reszka";
+        String publisher = "Agora";
+        String section = "dokument";
+        int year = 2020;
+        int number = 00001;
+
+        System.out.println(appName);
+        System.out.println("Książki dostępne w bibliotece:");
+        System.out.println(title);
+        System.out.println(author);
+
+    }
+}
